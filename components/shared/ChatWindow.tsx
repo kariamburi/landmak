48d59bb05cd3ca3c67e40fd70c995711bf40470a
Paul Irungu <paul.irungu@gmail.com>
2025-04-25 09:16:03 +0300

    first commit

--- components/shared/ChatWindow.tsx
+++ components/shared/ChatWindow.tsx
@@ -72,7 +72,7 @@ const ChatWindow: React.FC<ChatWindowProps> = ({
               // Fullscreen Popover for Mobile
       <div className="fixed h-screen inset-0 z-50 bg-white dark:bg-[#222528] dark:text-gray-100 p-0 flex flex-col">
         <div className="h-screen flex flex-col">
-        <div className="h-[50px] bg-gradient-to-r from-green-700 to-green-700 text-white p-2 rounded-t-lg">
+        <div className="h-[50px] bg-gradient-to-r from-green-700 to-green-700 text-white p-2">
         <div className="flex justify-between w-full">
           <h3 className="font-semibold text-white">{title !=="Home" && (<>{title}</>)}</h3>
           <div onClick={onClose} className="cursor-pointer text-white">
